c8efd530dc74f0c692886dca53d7924d8696ca43
achiko06 <achiko06@gmail.com>
2023-02-23 21:46:55 +0100

    Section title

--- components/shared/SectionTitle.tsx
+++ components/shared/SectionTitle.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
-const SectionTitle = () => {
+interface Props {
+  children: ReactNode
+  pretitle: string
+  title: string
+}
+
+const SectionTitle = (props:Props) => {
   return (
-    <div>SectionTitle</div>
-  )
+    <div className={`container p-8 mx-auto flex w-full flex-col mt-4 items-center justify-center text-center`}>
+      {props.pretitle && (
+        <div className="text-sm font-bold tracking-wider text-orange-500 uppercase">
+          {props.pretitle}
+        </div>
+      )}
+
+      {props.title && (
+        <h2 className="max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl dark:text-white">
+          {props.title}
+        </h2>
+      )}
+
+      {props.children && (
+        <p className="max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300">
+          {props.children}
+        </p>
+      )}
+    </div>
+    
+  );
 }
 
 export default SectionTitle
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { austronauts } from '@/utils/dummydata';
 import { MdCreate } from 'react-icons/md';
 import Wrapper from '@/components/shared/Wrapper';
 import Counter from '@/components/shared/Counter';
+import SectionTitle from '@/components/shared/SectionTitle';
 
 export default function Home() {
   const [activeCreateModal, setActiveCreateModal] = useState<Number>(0);
@@ -66,27 +67,37 @@ export default function Home() {
     <>
       <div>
         <Counter data={counterData}/>
-        <h1 className="bg-gray-900 text-white">Heading</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit voluptate maiores et impedit aliquam porro ratione temporibus neque recusandae eum repellat non nostrum, voluptas nihil labore dolores maxime sint assumenda.</p>
+
+        <SectionTitle
+          pretitle="EEA Database"
+          title="Austronauts">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit voluptate maiores et impedit aliquam porro ratione temporibus neque recusandae eum 
+        </SectionTitle>
         
-        <li className='flex flex-row space-x-5 py-1 pl-3 pr-4'>
+        <div className='flex flex-row space-x-5 py-1 pl-3 pr-4'>
           <button
             onClick={() => {activeCreateModal !== 1 ? setActiveCreateModal(1) : setActiveCreateModal(0)}} 
             className="text-green-600 font-bold hover:scale-125 hover:text-green-800 flex flex-row" 
           >
             <MdCreate/> Create 
           </button>
-        </li>
+        </div>
+
+        {/**MODALS */}
         <Create
           onClose={() => setActiveCreateModal(0)}
           isActive={activeCreateModal === 1}
           heading={"create profile"}
         />
+        {/**MODALS END */}
+
+        {/**MAIN TABLE */}
         <Wrapper 
         data={austronauts} 
         headers={titles}
         wrapperTitles={wrapperTitles}
         />
+        {/**MAIN TABLE END*/}
       </div>
     </>
   )
