266ce1d572401ae57a2e686f2b400fe69a3e055a
achiko06 <achiko06@gmail.com>
2023-02-26 21:09:47 +0100

    create api done

--- components/modals/Create.tsx
+++ components/modals/Create.tsx
@@ -1,5 +1,10 @@
 import { NextPage } from 'next'
 import React, { useState } from 'react'
+import { powers } from '@/utils/dummydata';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { BASE_URL } from '@/utils';
+
 
 interface IProps {
   isActive: Boolean;
@@ -8,23 +13,50 @@ interface IProps {
 }
 
 const Create: NextPage<IProps> = ({isActive, onClose, heading}) => {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [birthDate, setBirthDate] = useState('2023-07-01T18:19:00.000Z')
+  const [selectedPower, setSelectedPower] = useState<String>('superSpeed');
   const [savingPost, setSavingPost] = useState<Boolean>(false);
 
-  const powers = [
-    {
-      label: "Test",
-      _id: 1,
-    },
-    {
-      label: "Test2",
-      _id: 2,
-    },
-    {
-      label: "Test3",
-      _id: 3,
+  const router = useRouter();
+
+  const slugify = (str: string) =>
+    str
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, '')
+      .replace(/[\s_-]+/g, '-')
+      .replace(/^-+|-+$/g, '');
+  
+  const userProfile = true
+
+  const handlePost = async () => {
+    if (name && surname) {
+      setSavingPost(true);
+
+      if (userProfile) {
+        const doc = {
+          _type: 'person',
+          name,
+          surname,
+          birthDate,
+          power: selectedPower,
+          slug: { _type: 'slug', current: slugify(name+"-"+surname) }
+        };
+        await axios.post(`${BASE_URL}/api/item`, doc);
+       // alert(doc.name + doc.surname + " " + doc.birthDate)
+      } else {
+        alert("Not authorized to create a database entry, please login as admin")
+      }
+
+      router.push('/');
+      setSavingPost(false);
+      setName('');
+      setSurname('');
+      onClose();
     }
-  ];
-  const [selectedStatus, setSelectedStatus] = useState<String>('Test');
+  };
   
   return (
     <div className={`${isActive ? "" : "hidden"} fixed left-0 right-0 z-50 items-center justify-center  overflow-x-hidden overflow-y-auto top-4 md:left-1/4 md:top-24 xl:left-1/3 h-full`}>
@@ -52,8 +84,8 @@ const Create: NextPage<IProps> = ({isActive, onClose, heading}) => {
                         </label>
                         <input 
                           type="text"
-                         // value={title}
-                         // onChange={(e) => setTitle(e.target.value)}
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
                           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
                         />
                     </div>
@@ -64,8 +96,8 @@ const Create: NextPage<IProps> = ({isActive, onClose, heading}) => {
                         </label>
                         <input 
                           type="text"
-                         // value={title}
-                         // onChange={(e) => setTitle(e.target.value)}
+                          value={surname}
+                          onChange={(e) => setSurname(e.target.value)}
                           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
                         />
                     </div>
@@ -75,9 +107,9 @@ const Create: NextPage<IProps> = ({isActive, onClose, heading}) => {
                           Date of Birth
                         </label>
                         <input 
-                          type="text"
-                         // value={title}
-                         // onChange={(e) => setTitle(e.target.value)}
+                          type="date"
+                          value={birthDate}
+                          onChange={(e) => setBirthDate(e.target.value)} // + T18:19:00.000Z ?? test if be accepts without T
                           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
                         />
                     </div>
@@ -90,40 +122,22 @@ const Create: NextPage<IProps> = ({isActive, onClose, heading}) => {
                         </label>
                         <select
                         onChange={(e) => {
-                        //  setSelectedStatus(e.target.value);
+                        setSelectedPower(e.target.value);
                         }}
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
                         >
                           {powers.map((item) => (
                             <option
                               key={item._id}
-                              value={item.label}
+                              value={item.value}
                             >
-                              {item.label}
+                              {item.title}
                             </option>
                           ))}
                         </select>
                     </div>
                     </>
                     
-
-
-
-
-                    
-                    
-                    
-                    <div className="col-span-6">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                          Description
-                        </label>
-                        <textarea 
-                        rows={4} 
-                       // value={description}
-                      //  onChange={(e) => setDescription(e.target.value)}
-                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
-                        />
-                    </div>
                   </div> 
               </form>
             </div>
@@ -131,8 +145,8 @@ const Create: NextPage<IProps> = ({isActive, onClose, heading}) => {
             <div className="items-center space-x-1 p-6 border-t border-gray-200 rounded-b dark:border-gray-700">
                 
                 <button
-              //  disabled={title && description ? false : true}
-              //  onClick={handlePost}
+                disabled={name && surname ? false : true}
+                onClick={handlePost}
                 type='button' 
                 className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                 >
