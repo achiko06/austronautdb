ddaaf1e72f434f558a318357c98b831a07f7b4aa
achiko06 <achiko06@gmail.com>
2023-02-28 22:10:59 +0100

    Link element

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,5 +1,6 @@
 import { useSession, signIn } from 'next-auth/react';
 import Image from "next/image";
+import Link from 'next/link';
 import { useRouter } from "next/router";
 import { useState } from "react";
 import Logo from '../../utils/logo.png';
@@ -33,14 +34,14 @@ const Login = () => {
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <Link href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <Image 
             className="w-8 h-8 mr-2" 
             src={Logo}
             alt='logo'
           />
           EEA
-        </a>
+        </Link>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
